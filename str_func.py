25cc6380044af789f81a21604286c319b03b63cd
TsubaAnton <antontsuba2003@gmail.com>
2023-11-20 22:05:24 +0200

    Добавлена функция capitalize_words с docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def upper_word(input_str):
     return input_str.upper()
+
+
+def capitalize_words(input_str):
+    """Функция делает заглавными первые буквы каждого слова в строке"""
+    return ' '.join(word.capitalize() for word in input_str.split())
